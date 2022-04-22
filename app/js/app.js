f434ba69d84661cada969ea0d8eceeaa52647056
Sulleimenov <sulleimenov@yandex.ru>
2022-04-22 14:18:52 +0600

    edit pages

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,57 @@
 // import '~/app/vendor/mmenu/dist/mmenu.js'
+import { $ } from 'jquery'
+import { Fancybox } from "@fancyapps/ui";
+import mixitup from 'mixitup';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	/* lazy */
-	const lazyImages = document.querySelectorAll('img[data-src],source[data-srcset]')
-	const loadMapBlock = document.querySelector('.load-map')
-	const windowHeight = document.documentElement.clientHeight
+	// const tabs = document.querySelectorAll("[data-target]"),
+	// 			tabContents = document.querySelectorAll("[data-content]");
 
-	let lazyImagesPositions = []
-	if (lazyImages > 0) {
-		lazyImages.forEach(img => {
-			if (img.dataset.src || img.dataset.srcset) {
-				lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset)
+	// tabs.forEach((tab) => {
+	// 	tab.addEventListener("click", () => {
+	// 		const target = document.querySelector(tab.dataset.target);
+
+	// 		tabContents.forEach((tc) => {
+	// 			tc.classList.remove("is-active");
+	// 		});
+	// 		target.classList.add("is-active");
+
+	// 		tabs.forEach((t) => {
+	// 			t.classList.remove("is-active");
+	// 		});
+	// 		tab.classList.add("is-active");
+	// 	});
+	// });
+
+	let containerEl = document.querySelector('.rent-products')
+
+	if (containerEl) {
+		let mixer = mixitup(containerEl, {
+			"animation": {
+				"duration": 0,
+				"nudge": false,
+				"reverseOut": false,
+				"effects": ""
+			}
+		});
+	}
+
+	function burger() {
+		const btn = document.querySelector('.header__burger')
+		const nav = document.querySelector('.nav')
+
+		btn.addEventListener('click', function () {
+			if (nav.classList.contains('show')) {
+				nav.classList.remove('show')
+			} else {
+				nav.classList.add('show')
 			}
 		})
 	}
+	burger()
+
+	
+
+	Fancybox.bind("[data-fancybox]", {});
 })
